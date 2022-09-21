2b00012bc52f8afdfc2ff7598b0439caae186d83
Joshwin Greene <joshwin.greene@gmail.com>
2022-09-21 15:20:50 -0700

    feat(useLiveNotifications): Finished replacing the useState implementation with the reducer approach

--- src/features/notifications/hooks/useLiveNotifications.tsx
+++ src/features/notifications/hooks/useLiveNotifications.tsx
@@ -3,10 +3,10 @@ import { AppNotification } from 'common/models/notifications';
 import { environment } from 'environment';
 import { useAuth } from 'features/auth/hooks';
 import * as NotificationComponents from 'features/notifications/components';
-import { useEffect, useMemo, useReducer, useState } from 'react';
+import { useEffect, useMemo, useReducer } from 'react';
 import { toast } from 'react-toastify';
 
-const initialNotificationState = {
+const initialNotificationState: NotificationStateType = {
   notifications: [] as AppNotification[],
   oldNotifications: [] as AppNotification[],
   nextNotificationUrl: null
@@ -29,6 +29,8 @@ const notificationReducer = (state: NotificationStateType, action: NotificationA
   switch (action.type) {
     case 'clear':
       return initialNotificationState;
+    case 'logged out':
+      return { ...state, notifications: [] };
     case 'getMore':
       return { ...state, nextNotificationUrl: action.nextNotificationUrl ?? null };
     case 'received notification SSE':
@@ -45,23 +47,19 @@ export const useLiveNotifications = () => {
 
   const [notificationState, notificationDispatch] = useReducer(notificationReducer, initialNotificationState);
 
-  // const [notifications, setNotifications] = useState<AppNotification[]>([]);
-  // const [oldNotifications, setOldNotifications] = useState<AppNotification[]>([]);
-  // const [nextNotificationUrl, setNextNotificationUrl] = useState<string | null>(null);
-
   const {
     data: unreadNotifications,
     isFetching,
     isLoading,
-  } = useGetUnreadNotificationsQuery(nextNotificationUrl, {
+  } = useGetUnreadNotificationsQuery(notificationState.nextNotificationUrl, {
     skip: !user,
   });
 
   // Append new notifications that we got from the API to
   // oldNotifications list
   useEffect(() => {
-    setOldNotifications(o => [...o, ...(unreadNotifications?.results || [])]);
-  }, [unreadNotifications, setOldNotifications]);
+    notificationDispatch({ type: 'received new notifications', unreadNotifications: unreadNotifications?.results || [] });
+  }, [unreadNotifications]);
 
   // Set up receiving SSE events from the server.
   useEffect(() => {
@@ -72,7 +70,7 @@ export const useLiveNotifications = () => {
         console.log('message received');
         console.log('data:', message.data);
         const payload = JSON.parse(message.data);
-        setNotifications(n => [payload, ...n]);
+        notificationDispatch({ type: 'received notification SSE', notification: payload });
 
         // Grab the component for the notification type
         const Component = (NotificationComponents as any)[payload.type];
@@ -82,7 +80,7 @@ export const useLiveNotifications = () => {
       };
     } else {
       // user no longer logged in.
-      setNotifications([]);
+      notificationDispatch({ type: 'logged out' });
       if (eventSource) {
         (eventSource as EventSource).close();
         eventSource = null;
@@ -95,32 +93,30 @@ export const useLiveNotifications = () => {
         eventSource = null;
       }
     };
-  }, [user, setNotifications]);
+  }, [user]);
 
   const notificationProviderValue = useMemo(() => {
     console.log('RE-EVALUATE');
     const result = {
-      notifications: [...notifications, ...oldNotifications],
-      count: notifications.length + (unreadNotifications?.meta.count || 0), // I'm not so sure this is right.
+      notifications: [...notificationState.notifications, ...notificationState.oldNotifications],
+      count: notificationState.notifications.length + (unreadNotifications?.meta.count || 0), // I'm not so sure this is right.
       hasMore: !!unreadNotifications?.links.next,
       isFetching,
       isLoading,
       clear: () => {
-        setNotifications([]);
-        setNextNotificationUrl(null);
-        setOldNotifications([]);
+        notificationDispatch({ type: 'clear' });
         notificationApi.util.resetApiState();
         console.log('SETTING EVERYTHING TO NOTHING');
       },
       getMore: () => {
         if (unreadNotifications?.links.next && !isFetching) {
-          setNextNotificationUrl(unreadNotifications.links.next);
+          notificationDispatch({ type: 'getMore', nextNotificationUrl: unreadNotifications.links.next });
         }
       },
     };
     console.log(result);
     return result;
-  }, [notifications, unreadNotifications, oldNotifications, isFetching, isLoading]);
+  }, [unreadNotifications, isFetching, isLoading, notificationState.notifications, notificationState.oldNotifications]);
 
   return notificationProviderValue;
 };
